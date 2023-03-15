36a46ea3b89eeb9ae54a705de3bfc1d1c955c640
mariav1993 <126624816+mvaldez1993@users.noreply.github.com>
2023-03-15 23:13:45 +0000

    U3

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
+  ##################################################
+  # Comlete your code here
+  ##################################################
   email = input('Enter your string')
-  
+
   flag = True
   if not email[0].isalpha():
     flag = False
     lenemail = len(email)
-    if lenemail <= 5 or lenemail >= 30:
-      flag = False
+  if lenemail <= 5 or lenemail >= 30:
+    flag = False
   if email.find('@') == -1:
-      flag = False 
+    flag = False
   else:
     atidx = email.find('@')
-  if email [atidx+1:].find(".") == -1:
+  if email[atidx + 1:].find(".") == -1:
     flag = False
 
-  print (flag)
+  print(flag)
+
 
 if __name__ == '__main__':
-    main()
+  main()
